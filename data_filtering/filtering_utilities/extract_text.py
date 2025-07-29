fb6b151746801564efd289ba60d6ed5b205dcda3
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-29 13:26:04 +0300

    feat: Add function to extract HTML and convert it to text

---
+++ data_filtering/filtering_utilities/extract_text.py
@@ -0,0 +1,8 @@
+from resiliparse.parse.encoding import detect_encoding, bytes_to_str
+from resiliparse.extract.html2text import extract_plain_text
+
+
+def extract_text(html_bytes: bytes) -> str:
+    decoded = bytes_to_str(html_bytes, detect_encoding(html_bytes))
+    text = extract_plain_text(decoded, form_fields=True)
+    return text
