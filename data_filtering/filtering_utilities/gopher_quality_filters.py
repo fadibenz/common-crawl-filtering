bf7b6f440d1d97aa64a28e39e5e87a38df9cdd0e
fadi benzaima <f.benzaima@esi-sba.dz>
2025-07-30 14:29:02 +0300

    feat: Add more quality filters from the gopher paper

--- data_filtering/filtering_utilities/gopher_quality_filters.py
+++ data_filtering/filtering_utilities/gopher_quality_filters.py
@@ -21,10 +21,31 @@ def gopher_quality_filters(text: str)-> bool:
     if not (3<= mean_word_length <= 10):
         return False
 
-    # Contain less than 80% of words with at least one alphabetic character.
-    percentage_alpha = sum(any(c.isalpha() for c in word) for word in words) / length_words
+    percentage_alpha = 0
+    nb_symbols = 0
+    stop_words = ["the", "be", "to", "of", "and", "that", "have", "with"]
+    num_stop_words = 0
 
+
+    for word in words:
+        if any(c.isalpha() for c in word):
+            percentage_alpha +=  1 / length_words
+        if word.strip() in ["#", "..."]:
+            nb_symbols += 1
+        if word.lower() in stop_words:
+            num_stop_words += 1
+
+    # Contain less than 80% of words with at least one alphabetic character.
     if percentage_alpha < 0.8:
+        return False
+
+    # Symbol-to-word ratio greater than 0.1 for either the hash symbol or ellipsis
+    symbol_word_ratio = nb_symbols / length_words
+    if symbol_word_ratio > 0.1:
+        return False
+
+    # remove documents that do not contain at least two of the following English words
+    if num_stop_words < 2:
         return False
 
     # Have more than 30% of lines ending with an ellipsis (“...”).
