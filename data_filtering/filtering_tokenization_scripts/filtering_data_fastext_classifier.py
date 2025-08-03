6b0256b853498de5ec51306c6d470a9f41736c64
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-03 13:07:14 +0300

    feat: Add option to load unzipped file to match Kaggle expectations

--- data_filtering/filtering_tokenization_scripts/filtering_data_fastext_classifier.py
+++ data_filtering/filtering_tokenization_scripts/filtering_data_fastext_classifier.py
@@ -23,15 +23,15 @@ def parse_args():
 
     parser.add_argument("--overwrite", action="store_true", help="Overwrite output file if it exists")
 
-    parser.add_argument("--lang_model", type=str, help= "Path to language model classifier")
-    parser.add_argument("--nsfw_model", type=str, help= "Path to NSFW model classifier")
-    parser.add_argument("--hatespeech_model", type=str, help= "Path to hatespeech model classifier")
+    parser.add_argument("--lang_model", type=str, default="classifier_models/fasttext_language_ID.bin",  help= "Path to language model classifier")
+    parser.add_argument("--nsfw_model", type=str, default="classifier_models/jigsaw_fasttext_bigrams_nsfw_final.bin",help= "Path to NSFW model classifier")
+    parser.add_argument("--hatespeech_model", type=str, default="classifier_models/jigsaw_fasttext_bigrams_hatespeech_final.bin",help= "Path to hatespeech model classifier")
 
     parser.add_argument("--filter_lang", action="store_true", help="Filter records based on language")
     parser.add_argument("--lang", default="en", type=str, help="Language to filter for (e.g., 'en').")
-    parser.add_argument("--confidence", default=0.90, type=float, help="Minimum confidence score to keep a document.")
+    parser.add_argument("--confidence", default=0.60, type=float, help="Minimum confidence score to keep a document.")
     parser.add_argument("--good_quality", action="store_true", help="Good quality documents")
-    parser.add_argument("--max_len", type=int, default=10000, help="Max length for each document")
+    parser.add_argument("--max_len", type=int, default=50000, help="Max5 length for each document")
 
     return parser.parse_args()
 
@@ -60,8 +60,10 @@ if __name__ == "__main__":
     total_records = 0
     kept_records = 0
     logging.info(f"Processing WARC file: {input_file_path}")
-
-    stream = GZipStream(FileStream(str(input_file_path), "rb"))
+    if input_file_path.suffix == ".gz":
+        stream = GZipStream(FileStream(str(input_file_path), "rb"))
+    else:
+        stream = FileStream(str(input_file_path), "rb")
 
     with gzip.open(output_file, mode + "t", encoding="utf-8", errors="replace") as f:
         for record in ArchiveIterator(stream, record_types=WarcRecordType.response):
