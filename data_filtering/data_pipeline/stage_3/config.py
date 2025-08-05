f9a826ba26466247063fd5453f4f791f8aebc2c9
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-05 15:53:34 +0300

    feat: Add stage 3 of data pipeline; preforms parallel and memory-aware tokenization

---
+++ data_filtering/data_pipeline/stage_3/config.py
@@ -0,0 +1,31 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Pre-processing of CC files using Asyncio")
+
+    parser.add_argument("--input_file",
+                        required=True,
+                        type=str,
+                        help="Path to final pre-processed file")
+
+    parser.add_argument("--STAGE3_DIR",
+                        required=True,
+                        type=str,
+                        help="Stage 3 output directory")
+
+    parser.add_argument("--num_docs",
+                        type=int,
+                        default=10,
+                        help="Num of documents for each process at a time")
+
+    parser.add_argument("--num_workers",
+                        type=int,
+                        default=16,
+                        help="Num of workers for process loop")
+
+    parser.add_argument("--seed",
+                        default=2025,
+                        type=int,
+                        help="Seed for reproducibility.")
+
+    return parser.parse_args()
---
+++ data_filtering/data_pipeline/stage_3/main.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+from tqdm import tqdm
+from data_filtering.utils import setup_logging
+from data_filtering.data_pipeline.stage_3.config import parse_args
+from pathlib import Path
+from transformers import AutoTokenizer
+from concurrent.futures import ProcessPoolExecutor
+
+def _init():
+    global tok
+    tok = AutoTokenizer.from_pretrained("gpt2")
+
+def tokenize_line_and_add_eos(line):
+    return tok.encode(line) + [tok.eos_token_id]
+
+if __name__ == "__main__":
+    setup_logging()
+    args = parse_args()
+    out_file = Path(args.STAGE3_DIR) / "tokenized_training.bin"
+    num_workers = args.num_workers or os.cpu_count() or 1
+
+    with open(out_file, "wb") as f_out, \
+        open(args.input_file, "rt", encoding="utf-8") as f_in, \
+        ProcessPoolExecutor(max_workers=num_workers, initializer=_init) as exe:
+            for token_list in tqdm(exe.map(tokenize_line_and_add_eos,
+                                           f_in,
+                                           chunksize=args.num_docs),
+                                   desc="Tokenizing"
+                                   ):
+                f_out.write(np.array(token_list, dtype=np.uint16).tobytes())
