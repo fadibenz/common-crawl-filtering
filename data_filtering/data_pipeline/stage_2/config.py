190fe6da48f92ac8a7e2df9e1db1612cacb35b67
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-05 22:04:46 +0300

    feat: Add default values for CLI arguments

--- data_filtering/data_pipeline/stage_2/config.py
+++ data_filtering/data_pipeline/stage_2/config.py
@@ -17,23 +17,27 @@ def parse_args():
 
     parser.add_argument("--num_workers",
                         type=int,
-                        default=16,
+                        default=8,
                         help="Num of workers for process loop")
 
     parser.add_argument("--num_hashes",
                         type=int,
+                        default=128,
                         help="Number of hashes to use in MinHash")
 
     parser.add_argument("--num_bands",
                         type=int,
+                        default=16,
                         help="Number of bands to use in LSH")
 
     parser.add_argument("--num_grams",
                         type=int,
+                        default=3,
                         help="Number of grams to use")
 
     parser.add_argument("--jaccard_threshold",
                         type=int,
+                        default=0.8,
                         help="Threshold of jaccard threshold")
 
     parser.add_argument("--seed",
