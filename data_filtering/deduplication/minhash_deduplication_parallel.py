0b5b7e4e8f703a231e8b7b42cdb166298a771bbc
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-05 14:22:36 +0300

    feat: Optimize code by using batched reads and writes from the database

--- data_filtering/deduplication/minhash_deduplication_parallel.py
+++ data_filtering/deduplication/minhash_deduplication_parallel.py
@@ -6,27 +6,33 @@ import os
 from pathlib import Path
 from data_filtering.deduplication.utils import setup_db_connection, build_clusters, get_ngrams, compute_minhash_signature, compute_jaccard
 from tempfile import TemporaryDirectory
+import logging
 
 def lsh_candidates_sqlite(db_path,
                    num_bands:int
                    )-> Set[Tuple[str, str]]:
 
     conn = setup_db_connection(db_path)
-    cur = conn.execute("SELECT path, signature_list FROM signature")
     candidate_pairs_set = set()
 
     try:
-        for doc, signature in cur:
-            signature = json.loads(signature)
-            band_size = len(signature) // num_bands
+        batch_size = 10000
+        cur = conn.cursor()
+        cur.execute("SELECT path, signature_list FROM signature")
+        while True:
+            rows = cur.fetchmany(batch_size)
+            if not rows:
+                break
+            bands_to_insert = []
+            for doc, signature in rows:
+                signature = json.loads(signature)
+                band_size = len(signature) // num_bands
+                for i in range(0, len(signature), band_size):
+                    band = tuple(signature[i: i + band_size])
+                    bands_to_insert.append((json.dumps(band), doc))
 
-            for i in range(0, len(signature), band_size):
-                band = tuple(signature[i: i + band_size])
-                conn.execute("INSERT INTO bands(band, doc) VALUES(?, ?)", (json.dumps(band), doc))
-        conn.commit()
-        conn.close()
-
-        conn = setup_db_connection(db_path, read_only=True)
+            conn.executemany("INSERT INTO bands(band, doc) VALUES(?, ?)", bands_to_insert)
+            conn.commit()
 
         cur = conn.execute("""
             SELECT band, GROUP_CONCAT(doc)
@@ -46,41 +52,42 @@ def lsh_candidates_sqlite(db_path,
 
 def generate_signature_sqlite(path: str | os.PathLike,
                               num_hashes: int,
-                              num_grams: int,
-                              db_path: str | os.PathLike):
+                              num_grams: int):
 
-    conn = setup_db_connection(db_path)
-    try:
-        with open(path, "r", encoding="utf-8") as f:
-            text = f.read()
-        ngram_set = get_ngrams(text, num_grams)
-        signature = compute_minhash_signature(ngram_set, num_hashes)
+    with open(path, "r", encoding="utf-8", errors="ignore") as f:
+        text = f.read()
+    ngram_set = get_ngrams(text, num_grams)
+    signature = compute_minhash_signature(ngram_set, num_hashes)
+    return str(path), json.dumps(signature)
 
-        conn.execute(
-            """
-            INSERT INTO signature(path, signature_list) VALUES(?, ?)
-            """,
-            (str(path), json.dumps(signature))
-        )
-    finally:
-        conn.close()
 
 def confirm_pair(pair: Tuple[str, str], num_grams: int, threshold: float) -> Tuple[bool, Tuple[str, str]]:
     try:
         score = compute_jaccard(pair, num_grams)
         return score >= threshold, pair
     except Exception as e:
-        print(f"Failed pair {pair}: {e}")
+        logging.warning(f"Failed pair {pair}: {e}")
         return False, pair
 
 def copy_to_output(path: str, output_dir: str):
     try:
         output_path = Path(output_dir) / Path(path).name
-        with open(path, "r", encoding="utf-8") as f_in, open(output_path, "w", encoding="utf-8") as f_out:
+        with open(path, "r", encoding="utf-8", errors="ignore") as f_in, open(output_path, "w", encoding="utf-8") as f_out:
             f_out.write(f_in.read())
     except Exception as e:
-        print(f"Failed to copy {path}: {e}")
+        logging.warning(f"Failed to copy {path}: {e}")
 
+def insert_signatures(db_path, signatures_to_insert):
+    conn = setup_db_connection(db_path)
+    try:
+        conn.executemany(
+            """
+            INSERT INTO signature(path, signature_list) VALUES(?, ?)
+            """,
+            signatures_to_insert
+        )
+    finally:
+        conn.close()
 
 def minhash_deduplication_parallel(list_paths: List[str] | list[os.PathLike],
                           num_hashes: int,
@@ -91,31 +98,43 @@ def minhash_deduplication_parallel(list_paths: List[str] | list[os.PathLike],
                           num_workers: int = None):
 
     num_workers = num_workers or os.cpu_count() or 1
-
+    batch_size = 1000
 
     with TemporaryDirectory(prefix="dedup_") as tmp_root:
 
         db_path = Path(tmp_root) / "signatures.db"
         conn = setup_db_connection(db_path)
+
         try:
             conn.execute("CREATE TABLE IF NOT EXISTS signature(path TEXT PRIMARY KEY, signature_list TEXT )")
             conn.execute("CREATE TABLE IF NOT EXISTS bands(band TEXT , doc TEXT )")
+            conn.execute("CREATE INDEX IF NOT EXISTS idx_band ON bands(band)")
+            conn.execute("PRAGMA wal_checkpoint(FULL)")
+
         finally:
             conn.close()
 
         with ProcessPoolExecutor(max_workers=num_workers) as exe:
 
             futures = [
-                exe.submit(generate_signature_sqlite, path, num_hashes, num_grams, db_path)
+                exe.submit(generate_signature_sqlite, path, num_hashes, num_grams)
                 for path in list_paths
             ]
+            signatures_to_insert = []
 
             for fut in as_completed(futures):
                 try:
-                    fut.result()
-                except Exception as e:
-                    print(f"Worker for signature calculation failed: {e!r}")
+                    signatures_to_insert.append((fut.result()))
 
+                    if len(signatures_to_insert) >= batch_size:
+                        insert_signatures(db_path, signatures_to_insert)
+                        signatures_to_insert = []
+
+                except Exception as e:
+                    logging.error(f"Worker for signature calculation failed: {e!r}")
+
+        if len(signatures_to_insert) > 0:
+            insert_signatures(db_path, signatures_to_insert)
         candidate_pairs_set = lsh_candidates_sqlite(db_path, num_bands)
 
 
@@ -131,10 +150,10 @@ def minhash_deduplication_parallel(list_paths: List[str] | list[os.PathLike],
                 ok, pair = fut.result()
                 if ok: confirmed_pairs.add(pair)
             except Exception as e:
-                print(f"Worker for confirming pairs failed: {e!r}")
+                logging.error(f"Worker for confirming pairs failed: {e!r}")
 
 
-    duplicate_random = build_clusters(confirmed_pairs)
+    duplicate_random = build_clusters(confirmed_pairs) # Use DFS to build clusters
 
     all_paths = set(str(p) for p in list_paths)
     clustered_paths = set().union(*confirmed_pairs)
@@ -154,4 +173,4 @@ def minhash_deduplication_parallel(list_paths: List[str] | list[os.PathLike],
             for f in as_completed(futures):
                 f.result()
         except Exception as e:
-            print(f"Worker for copying files to output failed: {e!r}")
+            logging.error(f"Worker for copying files to output failed: {e!r}")
